21d1985031f7e25724d4c0524be548d0c44a8933
schrammbox <schrammbox@gmail.com>
2022-04-03 23:42:54 +0200

    Created UserService.java with methods: - addUser() - deleteUser() - getUserById and Email - getAllUsers() - check if Email already exists - encoding passwords for registered users.

--- WeBBAKI/src/main/java/de/thb/webbaki/repository/UserRepository.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/repository/UserRepository.java
@@ -4,10 +4,12 @@ import de.thb.webbaki.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = User.class, idClass = Long.class)
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findById(long id);
 }
