f75ab322f5932c4a14f1b93f2ba0232ab53f4828
schrammbox <schrammbox@gmail.com>
2022-04-04 14:38:07 +0200

    QuestionnaireService.java

--- WeBBAKI/src/main/java/de/thb/webbaki/service/QuestionnaireService.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/QuestionnaireService.java
@@ -1,5 +1,13 @@
 package de.thb.webbaki.service;
 
+import de.thb.webbaki.repository.QuestionaireRepository;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
+@Builder
 public class QuestionnaireService {
+    private final QuestionaireRepository questionaireRepository;
 }
