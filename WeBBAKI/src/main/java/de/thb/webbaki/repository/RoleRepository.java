8b0336e8d1355a14057b7c4487ccf8b91bb854a9
chris <sirhC-B>
2022-04-06 17:52:39 +0200

    Changes by Chris

--- WeBBAKI/src/main/java/de/thb/webbaki/repository/RoleRepository.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/repository/RoleRepository.java
@@ -4,10 +4,13 @@ import de.thb.webbaki.entity.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 
+import javax.swing.text.html.Option;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = Role.class, idClass = Long.class)
 public interface RoleRepository extends CrudRepository<Role, Long> {
 
-    Optional<Role> findByName(String name);
+    Role findByName(String name);
 }
