e80eb3692b5d0b5e85d897f141690224f8d4b093
chris <sirhC-B>
2022-04-12 13:36:24 +0200

    - reportpage/controller added

--- WeBBAKI/src/main/java/de/thb/webbaki/controller/QuestionnaireController.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/controller/QuestionnaireController.java
@@ -1,4 +1,25 @@
 package de.thb.webbaki.controller;
 
+import de.thb.webbaki.controller.form.QuestionnaireFormModel;
+import de.thb.webbaki.service.QuestionnaireService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
 public class QuestionnaireController {
+
+    private final QuestionnaireService questionnaireService;
+
+
+
+    @GetMapping("/report")
+    public String showQuestionnaireForm(Model model) {
+
+        model.addAttribute("questionnaire", new QuestionnaireFormModel());
+        return "report/create_report";
+    }
+
 }
