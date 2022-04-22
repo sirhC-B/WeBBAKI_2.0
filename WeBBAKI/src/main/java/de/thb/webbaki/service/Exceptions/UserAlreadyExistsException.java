c855bfc7d70b80edebc63ffc8baf2aeb49606466
Chris <70528080+sirhC-B@users.noreply.github.com>
2022-04-22 11:59:49 +0200

    Delete WeBBAKI directory

--- WeBBAKI/src/main/java/de/thb/webbaki/service/Exceptions/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package de.thb.webbaki.service.Exceptions;
-
-import javax.naming.AuthenticationException;
-
-public class UserAlreadyExistsException extends AuthenticationException {
-    public UserAlreadyExistsException(final String msg){
-        super(msg);
-    }
-}
