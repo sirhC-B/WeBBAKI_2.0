9e0f60cff5950980f611ebea5a19da63aeba2289
chris <sirhC-B>
2022-06-23 14:49:49 +0200

    some improvements

--- WeBBAKI/src/main/java/de/thb/webbaki/service/SnapshotService.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/SnapshotService.java
@@ -70,5 +70,76 @@ public class SnapshotService {
         return quests;
     }
 
-    
+    public Map<Long, Long[]> calcAverageValues(long snapID){
+        List<Questionnaire> quests = getAllQuestionnaires(snapID);
+        Map<Long, Long[]> finalMap = new HashMap<>();
+        int questcount = 0, prob = 0, imp = 0;
+
+        for(long i= 1; i <= 100; i++){
+            finalMap.put(i, new Long[] {(long)0,(long)0});
+        }
+
+        for(Questionnaire quest : quests){
+            Map<Long, String[]> map = questionnaireService.getMapping(quest);
+            questcount ++;
+            for(long i= 1; i <= map.size(); i++){
+                finalMap.put(i, new Long[] {finalMap.get(i)[0] + getProbNum(map.get(i)[0]),finalMap.get(i)[1] + getImpNum(map.get(i)[1])});
+            }
+        }
+        for(long i= 1; i <= finalMap.size(); i++){
+            finalMap.put(i, new Long[] {finalMap.get(i)[0] / questcount ,finalMap.get(i)[1] / questcount });
+        }
+
+        return finalMap;
+    }
+
+    public Map<Long, Long[]> createQuestMap(long snapID) {
+
+        return null;
+    }
+
+
+
+        public long getImpNum(String impValue){
+        long impNum = 0;
+
+        switch (impValue) {
+            case "keine":
+                impNum = 1;
+                break;
+            case "geringe":
+                impNum = 2;
+                break;
+            case "hohe":
+                impNum = 3;
+                break;
+            case "existenzielle":
+                impNum = 4;
+                break;
+        }
+
+        return impNum;
+    }
+    public long getProbNum(String probValue){
+        long probNum = 0;
+        switch (probValue) {
+            case "nie":
+                probNum = 0;
+                break;
+            case "selten":
+                probNum = 1;
+                break;
+            case "mittel":
+                probNum = 2;
+                break;
+            case "haeufig":
+                probNum = 3;
+                break;
+            case "sehr haeufig":
+                probNum = 4;
+                break;
+        }
+        return probNum;
+    }
+
 }
