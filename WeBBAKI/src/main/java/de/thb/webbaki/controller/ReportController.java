ddf8c583f415eea2126b8110f5b1b614343c52cf
sxBUTCHxs <leonschoenberg@gmx.de>
2022-09-27 16:24:24 +0200

    Pdf im Controller.

--- WeBBAKI/src/main/java/de/thb/webbaki/controller/ReportController.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/controller/ReportController.java
@@ -3,10 +3,24 @@ package de.thb.webbaki.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class ReportController {
     @GetMapping("report")
     public String showReport(){
       return "report";
+    }
+
+    @GetMapping("test")
+    public void getPdf(HttpServletResponse response){
+        try {
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.addHeader("Content-disposition", "attachment; filename=" + "employee.pdf");
+            response.setContentType("application/pdf");
+        }catch (Exception e){
+
+        }
     }
 }
