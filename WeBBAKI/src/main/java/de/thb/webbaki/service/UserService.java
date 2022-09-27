9d8ea1d7b0fce124e570e3ec52f1ccdff841238e
schrammbox <schrammbox@gmail.com>
2022-09-27 16:30:47 +0200

    Report saving works

--- WeBBAKI/src/main/java/de/thb/webbaki/service/UserService.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/UserService.java
@@ -13,6 +13,7 @@ import de.thb.webbaki.service.Exceptions.UserAlreadyExistsException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
