022e8c99fdbe27109999d953bede130160ab8103
sxBUTCHxs <leonschoenberg@gmx.de>
2022-09-30 10:06:44 +0200

    Add the Services for downloading the report-template as pdf.

--- WeBBAKI/src/main/java/de/thb/webbaki/controller/ReportController.java
+++ WeBBAKI/src/main/java/de/thb/webbaki/controller/ReportController.java
@@ -1,18 +1,60 @@
 package de.thb.webbaki.controller;
 
+import de.thb.webbaki.service.ReportService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.thymeleaf.context.Context;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class ReportController {
 
+    @Autowired
+    private ReportService reportService;
 
     @GetMapping("report")
-    public String showQuestionnaireForm(Model model,Authentication authentication) {
-        return "report";
+    public String showQuestionnaireForm(Model model) {
+        model.addAttribute("test", "Testiii");
+        return "report/report_container";
     }
 
+    @GetMapping("report/download")
+    public void downloadPdf(HttpServletResponse response){
+
+        response.setContentType("application/pdf");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=report.pdf";
+        response.setHeader(headerKey, headerValue);
+
+        Context context = new Context();
+        context.setVariable("test", "Testiii");
+
+        try {
+            reportService.generatePdfFromHtml(reportService.parseThymeleafTemplateToHtml("report/report", context),
+                    response.getOutputStream());
+        } catch (IOException e) {
+            //TODO add better error handling
+            e.printStackTrace();
+        }
+
+    }
 
 }
---
+++ WeBBAKI/src/main/java/de/thb/webbaki/service/ReportService.java
@@ -0,0 +1,46 @@
+package de.thb.webbaki.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+@Service
+public class ReportService {
+
+    /**
+     *
+     * @param template Does not need the .html.
+     * @param context Contains the variables that we want to be passed to Thymeleaf.
+     *                Addable with context.setVariable(key, value);
+     * @return the html code as a String
+     */
+    public String parseThymeleafTemplateToHtml(String template, Context context){
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine.process("templates/" + template, context);
+    }
+
+    public void generatePdfFromHtml(String html, OutputStream outputStream) throws IOException {
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        renderer.layout();
+        renderer.createPDF(bufferedOutputStream);
+
+        bufferedOutputStream.close();
+    }
+}
